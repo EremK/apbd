7369d63a06268a9ca0fd21344ba97dc994cf899b
s28391 <s28391@pjwstk.edu.pl>
2024-03-19 11:41:25 +0100

    LiquidContainer is implemented.

--- Lesson3/LiquidContainer.cs
+++ Lesson3/LiquidContainer.cs
@@ -2,5 +2,36 @@
 
 public class LiquidContainer : Container, IHazardNotifier
 {
+    public bool IsHazardous { get; set; }
 
+    public override void LoadCargo(double mass)
+    {
+        double maxMass = 0;
+
+        if (IsHazardous)
+        {
+            maxMass = MaxPayload * 0.5;
+        }
+        else
+        {
+            maxMass = MaxPayload * 0.9;
+        }
+        
+        if (mass > maxMass)
+        {
+            throw new OverfillException("An attempt to overfill a liquid container was spotted.");
+        }
+
+        CargoMass = mass;
+    }
+
+    public override void UnloadCargo()
+    {
+        CargoMass = 0;
+    }
+
+    public void SendHazardNotification(string message)
+    {
+        Console.WriteLine("Hazard! Serial number: " + SerialNumber + ", description" + message);
+    }
 }
